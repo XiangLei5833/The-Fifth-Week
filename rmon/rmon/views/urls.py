ea40672b0889ce71f280607b89e475a4abebc934
XiangLei <leixiang5833@163.com>
2017-12-15 12:01:30 +0800

    First-Day

---
+++ rmon/rmon/views/urls.py
@@ -0,0 +1,7 @@
+"""定义 API 对应的 URL"""
+
+from flask import Blueprint
+from rmon.views.index import IndexView
+
+api = Blueprint('api', __name__)
+api.add_url_rule('/', view_func=IndexView.as_view('index'))
