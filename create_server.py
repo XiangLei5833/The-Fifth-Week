b4ce395728ffd68e46684d180942d39198ff62d6
XiangLei <leixiang5833@163.com>
2017-12-26 14:42:11 +0800

    Third-challenge

---
+++ create_server.py
@@ -0,0 +1,12 @@
+import requests
+
+CREATE_SERVER = 'http://127.0.0.1:5000/servers/'
+
+def create_server(name, host):
+    data = {
+            'name': name,
+            'host': host
+            }
+    resp = requests.post(CREATE_SERVER, json=data)
+    return resp.json()
+
